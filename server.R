ccdfedd9d1d39fdb832104d91bffbe9354f709ba
voltain <michael@here.home>
2015-10-25 20:08:37 +0000

    rev 1

---
+++ server.R
@@ -0,0 +1,18 @@
+library(RecordLinkage)
+
+shinyServer(function(input, output) {
+  
+  
+
+    
+  
+  output$text <- renderText({
+    x<-(levenshteinSim(input$first, input$second))*100
+    if(input$submit > 0) {
+      paste("The word",input$first,"and",input$second,"are similar from", x,"percent")
+    }
+  })
+  
+
+})
+
---
+++ ui.R
@@ -0,0 +1,22 @@
+shinyUI(fluidPage(
+  titlePanel("Word Similarity with Levenshtein Distance"),
+  fluidRow(
+    column(12, wellPanel(
+      # Copy the line below to make a text input box
+      textInput("first", label = "Enter the first word: ", value = "wholemeal bread"),
+      textInput("second", label = "Enter the second word: ", value = "wholewheat bread"),
+      actionButton("submit", "Compare")
+    )),
+    column(12,
+           verbatimTextOutput("text")
+    )    ,
+    column(12,
+           h2('Documentation'),
+           p('In information theory and computer science, the Levenshtein distance is a metric for measuring the amount of difference between two sequences (i.e. an edit distance). The Levenshtein distance between two strings is defined as the minimum number of edits needed to transform one string into the other, with the allowable edit operations being insertion, deletion, or substitution of a single character.'),
+           p('The script takes the first word and compares with another, based on their distance it calculates the percentage.')
+    
+           )
+  )
+))
+
+
